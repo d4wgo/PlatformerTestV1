7bb2a134ea666ed94b5f835e11f281ea066abebf
doggo251 <37166660+doggo251@users.noreply.github.com>
2018-11-13 19:14:07 -0800

    First Commit

---
+++ dawgine.js
@@ -0,0 +1,688 @@
+/*
+goals:
+player 
+gameobjects -  position,size,type
+rayscanning
+canvas resizing and such
+gravity
+world positional differentiation
+*/
+//image refrences
+var background = new Image();
+background.src = "images/platformworld2PNG.png";
+var playerIdle = new Image();
+playerIdle.src = "images/player_idle.png";
+var playerJumpRight = new Image();
+playerJumpRight.src = "images/player_jumpRight.png";
+var playerJumpLeft = new Image();
+playerJumpLeft.src = "images/player_jumpLeft.png";
+var playerRun1Right = new Image();
+playerRun1Right.src = "images/player_walk1Right.png";
+var playerRun2Right = new Image();
+playerRun2Right.src = "images/player_walk2Right.png";
+var playerRun1Left = new Image();
+playerRun1Left.src = "images/player_walk1Left.png";
+var playerRun2Left = new Image();
+playerRun2Left.src = "images/player_walk2Left.png";
+//---------------
+var gameObjects = []; //gameobjects are seen by rayscans
+var nullObjects = []; //null objects are not seen by rayscans
+var buttons = []; //clickable buttons
+//gameObject
+//syntax:
+//new var newGO = GameObject(id,x,y,posX,posY,sizeX,sizeY);
+//gameObjects.push(newGO);
+class GameObject{
+    constructor(a,b,c,d,e){
+        this.id = a;
+        this.x = b;
+        this.y = c;
+        this.sizeX = d;
+        this.sizeY = e;
+        this.image = null;
+        this.color = null;
+        this.gravity = null;
+        this.rotation = null;
+        this.clicked = null;
+        this.hovered = null;
+        this.gravityTimer = 0;
+        this.yForce = 0;
+    }
+}
+function findObject(id){
+    for(var i = 0; i < gameObjects.length; i++){
+        if(gameObjects[i].id == id){
+            return gameObjects[i];
+        }
+    }
+    for(var i = 0; i < nullObjects.length; i++){
+        if(nullObjects[i].id == id){
+            return nullObjects[i];
+        }
+    }
+    for(var i = 0; i < buttons.length; i++){
+        if(buttons[i].id == id){
+            return buttons[i];
+        }
+    }
+    return null;
+}
+//rayscan
+//syntax:
+//rayscan(starting x, starting y, angle, distance)
+function rayscan(a,b,c,d){
+    var checkX = a;
+    var checkY = b;
+    var ang = c;
+    for(var i = 0; i < d; i++){
+        for(var j = 0; j < gameObjects.length; j++){
+            var objCheck = gameObjects[j];
+            if(checkX >= (objCheck.x - objCheck.sizeX/2) && checkX <= (objCheck.x + objCheck.sizeX/2) && checkY <= (objCheck.y + objCheck.sizeY/2) && checkY >= (objCheck.y - objCheck.sizeY/2)){
+                return objCheck;
+            }
+        }
+        checkX += Math.cos(ang);
+        checkY -= Math.sin(ang);
+    }
+    return null;
+}
+//input
+var input = {
+    w:false,
+    a:false,
+    s:false,
+    d:false,
+    up:false,
+    left:false,
+    down:false,
+    right:false,
+    space:false,
+    f:false,
+    shift:false,
+    one:false,
+    two:false,
+    three:false,
+    four:false,
+    five:false,
+    six:false,
+    seven:false,
+    mouse1:false
+}
+var clickInput = {
+    w:false,
+    a:false,
+    s:false,
+    d:false,
+    up:false,
+    left:false,
+    down:false,
+    right:false,
+    space:false,
+    f:false,
+    shift:false,
+    one:false,
+    two:false,
+    three:false,
+    four:false,
+    five:false,
+    six:false,
+    seven:false,
+    mouse1:false
+};
+var nClick;
+document.addEventListener('keydown', function(event) {
+    switch(event.code){
+        case "KeyW":
+            input.w = true;
+            clickInput.w = true;
+            break;
+        case "KeyA":
+            input.a = true;
+            clickInput.a = true;
+            break;
+        case "KeyS":
+            input.s = true;
+            clickInput.s = true;
+            break;
+        case "KeyD":
+            input.d = true;
+            clickInput.d = true;
+            break;
+        case "ArrowUp":
+            input.up = true;
+            clickInput.up = true;
+            break;
+        case "ArrowLeft":
+            input.left = true;
+            clickInput.left = true;
+            break;
+        case "ArrowDown":
+            input.down = true;
+            clickInput.down = true;
+            break;
+        case "ArrowRight":
+            input.right = true;
+            clickInput.right = true;
+            break;
+        case "Space":
+            input.space = true;
+            clickInput.space = true;
+            break;
+        case "KeyF":
+            input.f = true;
+            clickInput.f = true;
+            break;
+        case "ShiftLeft":
+            input.shift = true;
+            clickInput.shift = true;
+            break;
+        case "Digit1":
+            input.one = true;
+            clickInput.one = true;
+            break;
+        case "Digit2":
+            input.two = true;
+            clickInput.two = true;
+            break;
+        case "Digit3":
+            input.three = true;
+            clickInput.three = true;
+            break;
+        case "Digit4":
+            input.four = true;
+            clickInput.four = true;
+            break;
+        case "Digit5":
+            input.five = true;
+            clickInput.five = true;
+            break;
+        case "Digit6":
+            input.six = true;
+            clickInput.six = true;
+            break;
+        case "Digit7":
+            input.seven = true;
+            clickInput.seven = true;
+            break;
+    }
+});
+document.addEventListener('keyup', function(event) {
+    switch(event.code){
+        case "KeyW":
+            input.w = false;
+            break;
+        case "KeyA":
+            input.a = false;
+            break;
+        case "KeyS":
+            input.s = false;
+            break;
+        case "KeyD":
+            input.d = false;
+            break;
+        case "ArrowUp":
+            input.up = false;
+            break;
+        case "ArrowLeft":
+            input.left = false;
+            break;
+        case "ArrowDown":
+            input.down = false;
+            break;
+        case "ArrowRight":
+            input.right = false;
+            break;
+        case "Space":
+            input.space = false;
+            break;
+        case "KeyF":
+            input.f = false;
+            break;
+        case "ShiftLeft":
+            input.shift = false;
+            break;
+        case "Digit1":
+            input.one = false;
+            break;
+        case "Digit2":
+            input.two = false;
+            break;
+        case "Digit3":
+            input.three = false;
+            break;
+        case "Digit4":
+            input.four = false;
+            break;
+        case "Digit5":
+            input.five = false;
+            break;
+        case "Digit6":
+            input.six = false;
+            break;
+        case "Digit7":
+            input.seven = false;
+            break;
+    }
+});
+document.addEventListener('mousedown', function(event) {
+    input.mouse1 = true;
+    clickInput.mouse1 = true;
+});
+document.addEventListener('mouseup', function(event) {
+    input.mouse1 = false;
+});
+document.addEventListener('mousemove', function(event) {
+    getCursorPosition(canvas,event);
+});
+var mousePos = {
+    x:0,
+    y:0
+}
+//canvas creation
+var canvasName = "myCanvas"; //replace with id of canvas within the html
+var canvas = document.getElementById(canvasName);
+var ctx = canvas.getContext("2d");
+var virtualHeight = 900; //the width of the canvas things are being drawn on before scaling
+var virtualWidth = 1600; //the height of the canvas things are being drawn on before scaling
+fullScreen = false; //should the canvas fill the whole screen - make sure body and the canvas have a margin and padding of 0
+fitAspectRatioFullscreen = true; //should the aspect ratio of the virtual canvas be forced - this removes distortion of stretching
+fitDiv = false; //if you want the canvas to be in a part of the page instead of the whole page
+/*recomended css settings for canvas
+    padding:0;
+    margin-bottom:auto;
+    margin-top:auto;
+    margin-left:auto;
+    margin-right:auto;
+    display:block;
+*/
+var scaleX;
+var scaleY;
+setInterval(function(){
+    if(fullScreen){
+        fullScreenCanvas();
+    }
+    else if(fitAspectRatioFullscreen){
+        aspectRatioFullScreenCanvas();
+    }
+    else if(fitDiv){
+        fitDivCanvas();
+    }
+    scaleX = canvas.width / virtualWidth;
+    scaleY = canvas.height / virtualHeight;
+},1000/10); //refreshes canvas size a set times per second - the "10" is changeable to whatever tickrate works the best
+//canvas fit functions
+function fullScreenCanvas(){
+    canvas.width = window.innerWidth;
+    canvas.height =  window.innerHeight;
+}
+function aspectRatioFullScreenCanvas(){
+    var heightW = window.innerHeight;
+    var widthW = window.innerWidth;
+    var aspectR = virtualWidth / virtualHeight;
+    if(aspectR > widthW/heightW){
+        canvas.width = widthW;
+        canvas.height = widthW / aspectR;
+    }
+    else{
+        canvas.height = heightW;
+        canvas.width = heightW * aspectR;
+    }
+}
+function fitDivCanvas(){
+    var divIn = document.getElementById("myDIV"); //replace myDiv with the div the canvas is within
+    canvas.height = divIn.offsetHeight;
+    canvas.height = divIn.offsetWidth;
+}
+//cursor pos
+function getCursorPosition(canvas, event) {
+    var rect = canvas.getBoundingClientRect();
+    var x = event.clientX - rect.left;
+    var y = event.clientY - rect.top;
+    mousePos.x = x/scaleX;
+    mousePos.y = y/scaleY;
+}
+//game type:
+//overview - the player is in the middle of the screen, and moves around the world
+//non-follow overview, the player is in a world where the camera doesnt follow them, the whole game world is just the window
+//no player - used for games where the player is not a focal point, turned based stuff or anything like that
+var overview = false;
+var nFOverview = true;
+var nPO = false;
+var fov = { //fov is only used for overview follow games
+    x: 400,
+    y: 200
+}
+var presetWorldImage = false; //can be set to true or false
+var worldImage = new Image();
+worldImage.src = "http://logicsimplified.com/newgames/wp-content/uploads/2017/09/shovelknight.jpg"; //world background image replace with your own
+//the start function -  where every game object is made before the game starts
+var scene = 1;
+function start(){
+    scene = 1;
+    switchScene(scene);
+}
+start();
+var prevTime = Date.now();
+var delta;
+function runGame(){
+    delta = Date.now() - prevTime;
+    prevTime = Date.now();
+    for(var i = 0; i < buttons.length; i++){
+        var button = buttons[i];
+        if(mousePos.x <= (button.x + button.sizeX/2) && mousePos.x >= (button.x - button.sizeX/2) && mousePos.y >= (button.y - button.sizeY/2) && mousePos.y <= (button.y + button.sizeY/2)){
+            button.hovered = true;
+        }
+        else{
+            button.hovered = false;
+        }
+        if(button.hovered && clickInput.mouse1){
+            button.clicked = true;
+        }
+        else{
+            button.clicked = false;
+        }
+    }
+    Object.keys(clickInput).forEach(function(key) {
+        if(input[key] != clickInput[key]){
+            nClick = clickInput[key];
+        }   
+    });
+    nClick = false;
+    switch(scene){
+        case 1:
+            scene1(null);
+            break;
+        case 2:
+            scene2(null);
+            break;
+        case 3:
+            scene3(null);
+            break;
+        case 4:
+            scene4(null);
+            break;
+        case 5:
+            scene5(null);
+            break;
+        case 6:
+            scene6(null);
+            break;
+        case 7:
+            scene7(null);
+            break;
+        case 8:
+            scene8(null);
+            break;
+        case 9:
+            scene9(null);
+            break;
+    }
+    Object.keys(clickInput).forEach(function(key) {
+        clickInput[key] = false;     
+    });
+    draw();
+    window.requestAnimationFrame(runGame);
+}
+window.requestAnimationFrame(runGame);
+
+function draw(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    if(presetWorldImage){
+        if(!overview){
+            ctx.drawImage(worldImage,0,0,canvas.width,canvas.height);
+        }
+        else{
+            ctx.drawImage(worldImage,me.x - fov.x/2,me.y - fov.y/2,fov.x,fov.y,0,0,canvas.width,canvas.height);
+        }
+    }
+    for(var i = 0; i < nullObjects.length; i++){
+        var tempObject = nullObjects[i];
+        if(tempObject.gravity != null){
+            applyGravity(tempObject);
+        }
+        if(tempObject.color != null){
+            ctx.fillStyle = tempObject.color;
+            ctx.fillRect((tempObject.x - tempObject.sizeX/2) * scaleX,(tempObject.y - tempObject.sizeY/2) * scaleY,tempObject.sizeX * scaleX,tempObject.sizeY * scaleY);
+        }
+        else if(tempObject.image != null){
+            ctx.drawImage(tempObject.image,(tempObject.x - tempObject.sizeX/2) * scaleX,(tempObject.y - tempObject.sizeY/2) * scaleY,tempObject.sizeX * scaleX,tempObject.sizeY * scaleY);
+        }
+        
+    }
+    for(var i = 0; i < gameObjects.length; i++){
+        var tempObject = gameObjects[i];
+        if(tempObject.gravity != null){
+            applyGravity(tempObject);
+        }
+        if(tempObject.rotation != null){
+            ctx.translate(tempObject.x * scaleX,tempObject.y * scaleY);
+            ctx.rotate(tempObject.rotation);
+            ctx.translate(-tempObject.x * scaleX,-tempObject.y * scaleY);
+        }
+        if(tempObject.color != null){
+            ctx.fillStyle = tempObject.color;
+            ctx.fillRect((tempObject.x - tempObject.sizeX/2) * scaleX,(tempObject.y - tempObject.sizeY/2) * scaleY,tempObject.sizeX * scaleX,tempObject.sizeY * scaleY);
+        }
+        else if(tempObject.image != null){
+            ctx.drawImage(tempObject.image,(tempObject.x - tempObject.sizeX/2) * scaleX,(tempObject.y - tempObject.sizeY/2) * scaleY,tempObject.sizeX * scaleX,tempObject.sizeY * scaleY);
+        }
+        if(tempObject.rotation != null){
+            ctx.translate(tempObject.x * scaleX,tempObject.y * scaleY);
+            ctx.rotate(-tempObject.rotation);
+            ctx.translate(-tempObject.x * scaleX,-tempObject.y * scaleY);
+        }
+    }
+    for(var i = 0; i < buttons.length; i++){
+        var tempObject = buttons[i];
+        if(tempObject.color != null){
+            ctx.fillStyle = tempObject.color;
+            ctx.fillRect((tempObject.x - tempObject.sizeX/2) * scaleX,(tempObject.y - tempObject.sizeY/2) * scaleY,tempObject.sizeX * scaleX,tempObject.sizeY * scaleY);
+        }
+        else if(tempObject.image != null){
+            ctx.drawImage(tempObject.image,(tempObject.x - tempObject.sizeX/2) * scaleX,(tempObject.y - tempObject.sizeY/2) * scaleY,tempObject.sizeX * scaleX,tempObject.sizeY * scaleY);
+        }
+    }
+}
+function applyGravity(a){
+    if(rayscan(a.x,a.y + (a.sizeY / 2) + 1, 4.71, 2) == null){
+        a.gravityTimer += delta/1000;
+        a.y += a.gravity * a.gravityTimer;
+        if(a.id == "player"){
+            a.jump = true;
+        }
+    }
+    else{
+        a.gravityTimer = 0;
+        if(a.id == "player"){
+            a.jump = false;
+        }
+    }
+    if(a.yForce != 0){
+        a.y -= a.yForce * delta/8;
+        a.yForce -= delta/20;
+        if(a.yForce < 0){
+            a.yForce = 0;
+        }
+    }
+}
+function switchScene(a){
+    gameObjects = [];
+    nullObjects = [];
+    scene = a;
+    switch(a){
+        case 1:
+            scene1("start");
+            break;
+        case 2:
+            scene2("start");
+            break;
+        case 3:
+            scene3("start");
+            break;
+        case 4:
+            scene4("start");
+            break;
+        case 5:
+            scene5("start");
+            break;
+        case 6:
+            scene6("start");
+            break;
+        case 7:
+            scene7("start");
+            break;
+        case 8:
+            scene8("start");
+            break;
+        case 9:
+            scene9("start");
+            break;
+    }
+}
+var totalTime = 0;
+function scene1(a){
+    if(a == "start"){
+        //start function for scene1
+        nullObjects.push(new GameObject("bg",800,450,1600,900));
+        findObject("bg").image = background;
+        gameObjects.push(new GameObject("obj",719,228,431,30));
+        gameObjects.push(new GameObject("obj",105,700,211,28));
+        gameObjects.push(new GameObject("obj",550,576,423,33));
+        gameObjects.push(new GameObject("obj",1042,516,105,20));
+        gameObjects.push(new GameObject("obj",1493,406,212,18));
+        nullObjects.push(new GameObject("spawn",704,85,100,100));
+        //nullObjects.push(new GameObject("playerBG",704,85,100,130));
+        gameObjects.push(new GameObject("player",704,85,100,130));
+        //findObject("playerBG").color = "red";
+        findObject("player").image = playerIdle;
+        findObject("player").gravity = 9.8;
+        //findObject("playerBG").gravity = 9.8;
+        findObject("player").run = 0;
+        findObject("player").jumpUp = false;
+        findObject("player").jump = false;
+    }
+    else{
+        //logic for scene 1
+        totalTime += delta;
+        var me = findObject("player");
+        if(me.y > 950){
+            me.x = 704;
+            me.y = 85;
+        }
+        if(input.a){
+            me.run = -1;
+        }
+        else if(input.d){
+            me.run = 1;
+        }
+        else{
+            me.run = 0;
+        }
+        if(rayscan(me.x + (me.sizeX/2) + 1,me.y + (me.sizeY/2) - 10,0,3) != null || rayscan(me.x - (me.sizeX/2) - 1,me.y + (me.sizeY/2) - 10,3.14,3) != null){
+            me.run = 0;
+        }
+        if(clickInput.space && ((rayscan(me.x - (me.sizeX/2),me.y + (me.sizeY/2) + 3,4.71,2) != null) || (rayscan(me.x + (me.sizeX/2),me.y + (me.sizeY/2) + 3,4.71,2) != null))){
+            me.yForce = 16;
+        }
+        if(me.jump){
+            if(me.run < 0){
+                me.image = playerJumpLeft;
+            }
+            else{
+                me.image = playerJumpRight;
+            }
+        }
+        else if(me.run != 0){
+            if(me.run < 0){
+                if(totalTime < 200){
+                    me.image = playerRun1Left;
+                }
+                else{
+                    me.image = playerRun2Left;
+                }
+            }
+            else{
+                if(totalTime < 200){
+                    me.image = playerRun1Right;
+                }
+                else{
+                    me.image = playerRun2Right;
+                }
+            }
+        }
+        else{
+            me.image = playerIdle;
+        }
+        me.x += me.run * delta / 2;
+        if(totalTime > 400){
+            totalTime = 0;
+        }
+    }
+}
+function scene2(a){
+    if(a == "start"){
+    }
+    else{
+    }
+}
+function scene3(a){
+    if(a == "start"){
+
+    }
+    else{
+        
+    }
+}
+function scene4(a){
+    if(a == "start"){
+
+    }
+    else{
+        
+    }
+}
+function scene5(a){
+    if(a == "start"){
+
+    }
+    else{
+        
+    }
+}
+function scene6(a){
+    if(a == "start"){
+
+    }
+    else{
+        
+    }
+}
+function scene7(a){
+    if(a == "start"){
+
+    }
+    else{
+        
+    }
+}
+function scene8(a){
+    if(a == "start"){
+
+    }
+    else{
+        
+    }
+}
+function scene9(a){
+    if(a == "start"){
+
+    }
+    else{
+        
+    }
+}
+//game functions go down here
+
+
+
+
